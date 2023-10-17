256cd9aa44ebcb64d63ac8de3e708c76a78ed7ea
minmin <cindy65142650126508@gmail.com>
2023-10-17 10:13:21 +0800

    Create DBConfig.java

---
+++ src/main/java/hitstd/group/tool/database/DBConfig.java
@@ -0,0 +1,10 @@
+package hitstd.group.tool.database;
+
+public class DBConfig {
+	public DBConfig(){		
+	}
+	public String FilePath(){
+			String DBPath="C:\\Users\\cindy\\Desktop\\2023MS\\2023MS.accdb";
+		return DBPath;		
+	}
+}
