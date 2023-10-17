8d6ade313893df96d41f5bdcf330ec2cd0a5f27c
minmin <cindy65142650126508@gmail.com>
2023-10-17 15:20:54 +0800

    Update DBConfig.java

--- src/main/java/hitstd/group/tool/database/DBConfig.java
+++ src/main/java/hitstd/group/tool/database/DBConfig.java
@@ -4,7 +4,8 @@ public class DBConfig {
 	public DBConfig(){		
 	}
 	public String FilePath(){
-			String DBPath="C:\\Users\\cindy\\Desktop\\2023MS\\2023MS.accdb";
+			String DBPath="C:\\Users\\cindy\\Desktop\\2023MS\\src\\main\\webapp\\2023MS.accdb";
+			//String DBPath="C:\\Users\\cindy\\Downloads\\產後護理之家\\2023MS.accdb";
 		return DBPath;		
 	}
 }
